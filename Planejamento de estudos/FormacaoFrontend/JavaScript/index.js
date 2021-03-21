9ee30266a12cca0b47be3ffea44d0eb2273ec09e
AndreSntos <andre.santos@bandtec.com.br>
2021-03-21 14:24:07 -0300

    Novos projetos

--- Planejamento de estudos/FormacaoFrontend/JavaScript/index.js
+++ Planejamento de estudos/FormacaoFrontend/JavaScript/index.js
@@ -1,14 +1,16 @@
 import { Cliente } from "./Cliente.js";
-import { contaCorrente } from "./ContaCorrente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
+const diretor = new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456");
+const gerente = new Gerente("Ricardo", 5000, 12345678901);
+gerente.cadastrarSenha("123");
+const cliente = new Cliente("Lais", 789465612379, "456");
 
-const contaCorrenteRicardo = new contaCorrente(1001, cliente1);
-contaCorrenteRicardo.depositar(500);
-const conta2 = new contaCorrente(1001, cliente2);
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-let valor = 200;
-contaCorrenteRicardo.transferir(200, conta2);
-
-console.log(contaCorrente.numeroDeContas)
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
