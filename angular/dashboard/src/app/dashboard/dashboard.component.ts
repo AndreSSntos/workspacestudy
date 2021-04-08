c0b8ec6d892d40a9086ebae83ecd14439d40790d
AndreSntos <andre.santos@bandtec.com.br>
2021-04-08 14:51:16 -0300

    V1 da calculadora

--- angular/dashboard/src/app/dashboard/dashboard.component.ts
+++ angular/dashboard/src/app/dashboard/dashboard.component.ts
@@ -24,24 +24,22 @@ export class DashboardComponent implements OnInit {
     );
   }
 
-  init(): void{
-      if(typeof(google) !== 'undefined'){
-        google.charts.load('current', {
-          'packpages': ['corechart']
-        });
-        setTimeout(() => {
-          google.charts.setOnLoadCallback(this.exibirGraficos()); 
-        }, 1000) 
-      }
+ init(): void {
+    if(typeof(google) !== 'undefined') {
+      google.charts.load('current', {'packages':['corechart']});
+      setTimeout(() => {
+      	google.charts.setOnLoadCallback(this.exibirGraficos());
+      }, 1000);
+    }
   }
 
   exibirGraficos(): void{
     this.exibirPieChart();
-    // this.exibir3dPieChart();
-    // this.exibirBarChart();
-    // this.exibirLineChart();
-    // this.exibirColumnChart();
-    // this.exibirDonutChart();
+    this.exibir3dPieChart();
+    this.exibirBarChart();
+    this.exibirLineChart();
+    this.exibirColumnChart();
+    this.exibirDonutChart();
 
   }
   
@@ -52,12 +50,43 @@ export class DashboardComponent implements OnInit {
 
     chart.draw(this.obterDataTable(), this.obterOpcoes()); 
   }
-  // exibir3dPieChart(): void{
-  //   const el = document.getElementById('3d_pie_chart');
-  //   const chart = new google.visualization.PieChart(el);
+  exibir3dPieChart(): void{
+    const el = document.getElementById('3d_pie_chart');
+    const chart = new google.visualization.PieChart(el);
+    const opcoes = this.obterOpcoes();
 
-  //   chart.draw(this.obterDataTable(), this.obterOpcoes()); 
-  // }
+
+    opcoes['is3D'] = true;
+    chart.draw(this.obterDataTable(), opcoes); 
+  }
+  exibirBarChart(): void{
+    const el = document.getElementById('bar_chart');
+    const chart = new google.visualization.BarChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes()); 
+  }
+  exibirLineChart(): void{
+    const el = document.getElementById('line_chart');
+    const chart = new google.visualization.LineChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes()); 
+  }
+  exibirColumnChart(): void{
+    const el = document.getElementById('column_chart');
+    const chart = new google.visualization.ColumnChart(el);
+
+    chart.draw(this.obterDataTable(), this.obterOpcoes()); 
+  }
+  exibirDonutChart(): void{
+    const el = document.getElementById('donut_chart');
+    const chart = new google.visualization.PieChart(el);
+    const opcoes = this.obterOpcoes();
+
+
+    opcoes['pieHole'] = 0.4;
+    chart.draw(this.obterDataTable(), opcoes); 
+  }
+
 
   obterDataTable(): any{
     const data = new google.visualization.DataTable();
